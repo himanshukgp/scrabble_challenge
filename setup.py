1a198c641d114bcd82f16b7a8502d2b0aff32e6e
himanshukgp <hs80941@gmail.com>
2017-10-11 18:44:42 +0530

    get the rack from cmd line arg

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+import sys
+
 with open('sowpods.txt') as f:
 	mylist = list(map(str.strip, f))
 
-print(mylist)
+rack = sys.argv
+rack = rack[1:]
+#print (rack)
+if len(rack)==0:
+	sys.exit('Error!\n Supply some rack')
+for element in rack:
+	if element.isupper()==False:
+		sys.exit('Error!\n all rack must be in capital letters')
 
 
 
